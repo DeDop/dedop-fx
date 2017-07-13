b8a4a65950bd37d1e1a153cf321bb78684d57357
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-07-13 15:03:30 +0200

    update

--- mkwin.bat
+++ mkwin.bat
@@ -4,9 +4,17 @@ set JRE_HOME=C:\Program Files\Java\jre1.8.0_131
 "%JDK_HOME%\bin\javapackager.exe" -createjar -v ^
    -outdir build ^
    -outfile DeDopFX ^
-   -srcdir out\production\dedop-fx ^
+   -srcdir out\production ^
    -appclass dedopfx.ui.App ^
    -classpath netcdfAll-4.6.6.jar
+
+"%JDK_HOME%\bin\javapackager.exe" -signjar -v ^
+   -outdir build ^
+   -keyStore "%JKS_FILE%" ^
+   -storePass %JKS_PASSWORD% ^
+   -alias tools ^
+   -keypass %JKS_TOOLS_PASSWORD% ^
+   -srcdir build
 
 "%JDK_HOME%\bin\javapackager.exe" -deploy -v ^
    -native installer ^
@@ -26,3 +34,5 @@ set JRE_HOME=C:\Program Files\Java\jre1.8.0_131
    -Bicon=src\dedopfx\resources\dedop.ico ^
    -Bidentifier=forman.dedopfx
 
+signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "%CSC_FILE%" /p %CSC_KEY_PASSWORD% dist/bundles/DeDopFX-0.1.exe
+signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f "%CSC_FILE%" /p %CSC_KEY_PASSWORD% dist/bundles/DeDopFX-0.1.msi
